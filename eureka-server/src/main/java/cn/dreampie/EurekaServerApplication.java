fccbb5a565c75dfad25d9907ed8d202b4ce9b9b4
Dreampie <Dreampie@outlook.com>
2015-12-01 17:31:03 +0800

    init eureka server

--- eureka-server/src/main/java/cn/dreampie/EurekaServerApplication.java
+++ eureka-server/src/main/java/cn/dreampie/EurekaServerApplication.java
@@ -2,7 +2,11 @@ package cn.dreampie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableDiscoveryClient
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 

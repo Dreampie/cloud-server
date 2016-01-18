683e519a5ca9fbd883ae34d261c7a79c085c3f6d
Dreampie <Dreampie@outlook.com>
2016-01-18 11:02:37 +0800

    update dependency

---
+++ hystrix-dashboard/src/main/java/tv/acfun/cloud/server/HystrixDashboardApplication.java
@@ -0,0 +1,23 @@
+package tv.acfun.cloud.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@EnableHystrixDashboard
+@SpringBootApplication
+@Controller
+public class HystrixDashboardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HystrixDashboardApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public String home() {
+    return "forward:/hystrix";
+  }
+}
